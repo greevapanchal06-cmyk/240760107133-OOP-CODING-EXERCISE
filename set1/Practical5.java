4b83f138dbe4c2e2f77a86cf167fc701352169d3
greevapanchal06-cmyk <greeva.panchal06@gmail.com>
2026-04-18 10:25:16 +0530

    calculate area of triangle

---
+++ set1/Practical5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter three sides of triangle: ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        // Check if valid triangle
+        if (a + b > c && a + c > b && b + c > a) {
+            double s = (a + b + c) / 2;
+
+            double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+            System.out.println("Area of triangle = " + area);
+        } else {
+            System.out.println("Invalid triangle sides");
+        }
+    }
+}
